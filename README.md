2493ec6ec883df832ccbf915124188fced7efea2
ImCzf#2333 <Czf_233@outlook.com>
2023-07-02 21:44:13 +0800

    L

---
+++ README.md
@@ -0,0 +1,7 @@
+# PridePlus Fucker
+
+Auto Edit PridePlus ClientName (Bypass AntiEdit)
+
+自动修改PridePlus相关名称（绕过反修改字符串）
+
+神权狗你可以去死了。
---
+++ src/main/java/cc/paimonmc/jbyte/FxUtil.java
@@ -0,0 +1,21 @@
+package cc.paimonmc.jbyte;
+
+
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+
+public class FxUtil {
+    public static File openFileChooser(String title,String desc){
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle(title);
+        fileChooser.setInitialDirectory(new File(System.getProperty("user.home") + "/Desktop"));
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter(desc, "*." + desc)
+        );
+        Stage stage = new Stage();
+        return fileChooser.showOpenDialog(stage);
+    }
+}
